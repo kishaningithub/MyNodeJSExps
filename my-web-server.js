42f2e50ee80828ce34f7632f6c56ede171f43fcf
Kishan.B <kishancs46@gmail.com>
2014-02-09 20:50:02 +0530

    Done a time out and ran a test
    Test - #using apache benchmark
      ab -n 100 -c 100 http://localhost:8000/

--- my-web-server.js
+++ my-web-server.js
@@ -1,6 +1,9 @@
 var http_mod=require("http")
 var s=http_mod.createServer(function(req,res){
 	res.writeHead(200,{"content-type":"text-plain"});
-	res.end("hello kishan");
+	res.write("hello\n");
+	setTimeout(function(){
+		res.end("kishan\n");
+	},2000);
 });
 s.listen(8000);
